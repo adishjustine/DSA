74527e096dc80dad49d386fdb2a09aa9448d0051
adishjustine <156789207+adishjustine@users.noreply.github.com>
2025-10-26 20:54:36 +0530

    Solution 1768: Merge Strings Alternatively

--- Leetcode75/Array_String/1768_MergeStringsAlternately.cpp
+++ Leetcode75/Array_String/1768_MergeStringsAlternately.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int length1= word1.size();
-        int length2= word2.size();
-        
-        string result;
+        int size1 = word1.size();  // original size of word1
+        int size2 = word2.size();  // original size of word2
+        int ptr1 = 0;
+        int ptr2 = 0;
 
-        for (int i=0; i< length1|| i< length2; ++i){
-            if (i< length1){
-                result+= word1[i];
-            }
-            if (i< length2){
-                result+= word2[i];
-            }
+        // Use current word1.size() in the loop to handle growing string
+        while (ptr1 < word1.size() && ptr2 < size2) {
+            ptr1++;  // move past current character
+            word1.insert(ptr1, 1, word2[ptr2]);  // insert one char from word2
+            ptr1++;  // move past inserted char
+            ptr2++;
         }
 
-        return result;
-        
+        // Append any leftover characters from word2 if it was longer originally
+        if (size2 > size1) {
+            word1 += word2.substr(ptr2);
+        }
+
+        return word1;
     }
 };
